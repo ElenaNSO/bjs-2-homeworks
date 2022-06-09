40293dbe2601979e9d9feb7752d1e76f09ad07d8
ElenaNSO <yaelena@ngs.ru>
2022-06-09 19:42:05 +0700

    feat: add solution of a quadratic equation

--- 1.base-concepts/task.js
+++ 1.base-concepts/task.js
@@ -1,6 +1,29 @@
+"use strict";
+
 function solveEquation(a, b, c) {
   let arr;
-  // код для задачи №1 писать здесь
+  let discriminant, squareRoot;
+
+  arr = [];
+
+  discriminant = b ** 2 - 4 * a * c;
+
+  if(discriminant === 0) {
+    squareRoot = - b / (2 * a);
+    arr.push(squareRoot);
+   }
+   else {
+     if (discriminant > 0) {
+       console.log(Math.sqrt(discriminant));
+       console.log(- b + Math.sqrt(discriminant));
+       console.log(2 * a);
+      squareRoot = (- b + Math.sqrt(discriminant)) / (2 * a);
+      arr.push(squareRoot);
+      squareRoot = (- b - Math.sqrt(discriminant)) / (2 * a);
+      arr.push(squareRoot);
+     }
+   }
+   // код для задачи №1 писать здесь
   return arr; // array
 }
 
