f752506814e0739c5ed63d31bc505bfe4eef9f2e
ElenaNSO <yaelena@ngs.ru>
2022-06-15 19:44:07 +0700

    feat: add task solving, 1 and 2

--- 3.arrays/task.js
+++ 3.arrays/task.js
@@ -1,7 +1,15 @@
 function compareArrays(arr1, arr2) {
   let result;
 
-  // Ваш код
+  result = false;
+
+  function elementEqualityough(element, index) {
+    return element === arr2[index];
+  };
+
+  if (arr1.length === arr2.length) {
+    result = arr1.every(elementEqualityough) 
+  }
 
   return result; // boolean
 }
@@ -9,7 +17,7 @@ function compareArrays(arr1, arr2) {
 function advancedFilter(arr) {
   let resultArr;
 
-  // Ваш код
-
+  resultArr = arr.filter(arr => (arr > 0) && (arr % 3 ===0)).map(arr => arr * 10);
+  
   return resultArr; // array
 }
