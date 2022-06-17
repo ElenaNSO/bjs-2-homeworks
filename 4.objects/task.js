4e964d0445abb8f6e16ad2fc1eef145c0ad50865
ElenaNSO <yaelena@ngs.ru>
2022-06-17 18:23:12 +0700

    feat: add solution of the task 1 (4.objects)

--- 4.objects/task.js
+++ 4.objects/task.js
@@ -1,10 +1,51 @@
 function Student(name, gender, age) {
-    // Ваш код
-
+  let student;
+   
+  this.name = name;
+  this.gender = gender;
+  this.age = age; 
 }
 
-Student.prototype.setSubject = function (subjectName) {
-  //ваш код
+const student1 = new Student ('Даша','женский',19);
+const student2 = new Student ('Ольга','женский',20); 
+const student3 = new Student ('Саша','мужской',19); 
+const student4 = new Student ('Андрей','мужской',18); 
+
+Student.prototype.setSubject = function(subjectName) {
+  this.subject = subjectName;
 }
 
-// ваш код для остальных методов
+Student.prototype.addMark = function (mark) {
+  if (this.marks === undefined) {
+    this.marks = [];
+    this.marks.push(mark);
+  }
+  else{
+    this.marks.push(mark);
+  }
+}
+
+Student.prototype.addMarks = function(...mark) {
+  if (this.marks === undefined) {
+    this.marks = [];
+    }
+  for(let i of mark) {
+    this.marks.push(i);
+  }
+}
+
+Student.prototype.getAverage = function() {
+  const average = this.marks.reduce((acc, mark) => {
+    return acc + mark;
+    })
+  
+  return  average / this.marks.length;    
+};
+
+Student.prototype.exclude = function(reason) {
+  delete Student.prototype.subject;
+  delete Student.prototype.marks;
+
+  this.excluded = reason;
+}
+
