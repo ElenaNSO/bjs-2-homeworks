d84c3a60e73c10e98c8b038a85bbafb16cf63d65
ElenaNSO <yaelena@ngs.ru>
2022-07-03 15:55:28 +0700

    feat: add  task.js

--- 7.async/task.js
+++ 7.async/task.js
@@ -2,55 +2,101 @@ class AlarmClock {
     constructor(time, id, callback, setInterval)
     {
         this.alarmCollection = [];
-        this.timerId = id;
-        this.callback = callback;
-        this.time = time;
-        //this.timeMS = this.time.getTimes() - getTimes(new Date());
-        this.setInterval = setInterval;
-
+        this.timerId = null;
     }
-
-    addClock (time, id, callback){
+    
+  addClock (time, callback, id){
         if(!id){
-            const errorOut = new Error('error text');
+            const errorOut = new Error('Невозможно идентифицировать будильник. Параметр id не передан.');
             throw errorOut;
         }
-        if(this.alarmCollection.filter(id)) {
-            console.error(`d = $id уже существует`)
-            
-        }
-        else {
-            this.alarmCollection.push({time, id, callback}) 
-        }
         
+        function findId(element) {
+            if(element.id === id){
+                return true;   
+            }
+        }
+
+        try{
+            if(this.alarmCollection.find(findId)){
+                console.error(`будильник с таким id = ${id} уже существует.`);
+            }
+            else {
+                this.alarmCollection.push({time:time, id:id,callback:callback }); 
+            }
+        }
+        catch(errorOut){
+            return errorOut;
+        }
     }
 
     removeClock(id){
-        return delete AlarmClock.filter[id];
+        const lengtArrBeforDelete = this.alarmCollection.length;
+       
+        function findId(element) {
+            if(element.id === id){
+                return element;   
+            }
+        }
+        
+        this.alarmCollection.splice(this.alarmCollection.findIndex(findId),1)
+        const lengtArrAfterDelete = this.alarmCollection.length;
+        const resoltDelete = (lengtArrBeforDelete !== lengtArrAfterDelete);
+        return resoltDelete;
     }
 
     getCurrentFormattedTime() {
-       const timeHoursMinutes = new Date();
-
-       return timeHoursMinutes.getHours() + ":" + timeHoursMinutes.getMinutes();
-
+       const timeHoursMinutesNow = (new Date().toTimeString()).slice(0,5);
+        return  timeHoursMinutesNow;
     }
 
-    start(){
-        function checkClock(alarmCollection) {
-            alarmCollection(time => {
-               if(time == getCurrentFormattedTime){
-                   return alarmCollection.callback;
-               }
-               else return false;
-           });
+   start(time){
+        const thisAlarmCollection = this.alarmCollection;
+        const timeHoursMinutesNow = this.getCurrentFormattedTime();
+               
+        function findTime(element) {
+            if(element.time == time){
+                return element;   
+            } 
+        }
 
-        function setInterval(alarmCollection){
-           //const minTimeMS = Math.min(alarmCollection.timeMS);
+        function checkClock(element) {
+            if(element.time == timeHoursMinutesNow){
+                thisAlarmCollection[thisAlarmCollection.findIndex(findTime)].callback();
+            }
+        };
+
+        if(this.timerId == null) {
+            this.timerId = 1000;
+        }
+
+        function forEachCall(){
+            thisAlarmCollection.forEach(checkClock);
+        }
+
+        this.timerId = setInterval(forEachCall, this.timerId) 
+    }
+
+    stop(){
+        if(this.timerId) {
+            clearInterval(this.timerId);
+            this.timerId = null;
+        }
+    }
+
+    printAlarms() {
+        function printCall(element) {
+            console.log(`Будильник № ${element.id} заведен на ${element.time}`)   
 
         }
 
-        }
+        console.log(`Печать всех будильников в количестве ${element.lenght}`);
+        this.alarmCollection.forEach(printCall);
+    }
 
+    clearAlarms(){
+        clearInterval(this.timerId);
+        this.timerId = null;
+        this.alarmCollection = []; 
     }
 }
